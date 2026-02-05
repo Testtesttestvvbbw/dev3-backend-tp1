b85315bfd03f3b6fb76179d6e686e8c1d3381998
Timothée Cuny <timothee.cuny@gmail.com>
2026-02-05 21:59:13 +0100

    commit 6

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,24 @@ import express, { type Request, type Response } from "express";
 const app = express();
 const PORT = 3000;
 
+
+
+//Code ajouter pour la q6
+type Etudiant = { id: number; nom: string; prenom: string };
+
+const etudiants: Etudiant[] = [
+  { id: 1, nom: "Dupont", prenom: "Jean" },
+  { id: 2, nom: "Martin", prenom: "Sophie" },
+  { id: 3, nom: "Doe", prenom: "John" },
+];
+
+app.get("/api/data", (req: Request, res: Response) => res.json(etudiants));
+
+
+
+
+
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Bienvenue sur mon serveur API");
 });
