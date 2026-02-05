1dbe66514f3f584c7c461c08d578a3f17f95aa38
Timothée Cuny <timothee.cuny@gmail.com>
2026-02-05 21:41:24 +0100

    commit 4

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,12 @@
-function greet(name: string): string {
-  return "Hello World";
-}
-console.log(greet("Timothée"));
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Bienvenue sur mon serveur API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
